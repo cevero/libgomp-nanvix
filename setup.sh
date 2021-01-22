cc6e83f785eb955176f7db154822b7b837b8406c
reinaldoasf <reinaldoasf@gmail.com>
2021-01-22 00:53:26 -0300

    ignored setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 export WORKDIR=$HOME/workspace
 export TARGET=qemu-riscv32
-export PATH=$PATH:$WORKDIR/reinaldo-libgomp-nanvix/utils/toolchain/qemu/bin
+export PATH=$PATH:$WORKDIR/libgomp-nanvix/utils/toolchain/qemu/bin
