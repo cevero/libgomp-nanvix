a7151534d4cfb1e293bd8a4abe7f003918242e59
reinaldoasf <reinaldoasf@gmail.com>
2021-02-06 11:56:10 -0300

    created alias to gdb on setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ export PATH=$PATH:$WORKDIR/reinaldo-libgomp-nanvix/utils/toolchain/qemu/bin
 export OMP_NUM_THREADS=4
 export OMP_THREAD_LIMIT=4
 export RISCVGDB=$WORKDIR/reinaldo-libgomp-nanvix/utils/toolchain/riscv32/bin/riscv32-elf-gdb
+alias rgdb=$RISCVGDB
